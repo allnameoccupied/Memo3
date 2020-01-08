2b14cc2f4d7e4259b56549b6c79ee80763c1c5d6
ethwer <32164324+ethwer@users.noreply.github.com>
2020-01-09 01:51:51 +0800

    qwer

--- app/src/main/java/com/max/memo3/TestSubject/Test4_Firestore.java
+++ app/src/main/java/com/max/memo3/TestSubject/Test4_Firestore.java
@@ -1,30 +1,47 @@
 package com.max.memo3.TestSubject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.Task;
+import com.max.memo3.Confidential.Conf_Info;
 import com.max.memo3.R;
 import com.max.memo3.Util.util;
-
-import java.util.TimerTask;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+//google sign in for android https://developers.google.com/identity/sign-in/android/start-integrating
 public class Test4_Firestore extends Fragment {
     //var
     private Test_ViewModel viewModel;
+    private GoogleSignInOptions googleSignInOptions;
+    private GoogleSignInClient googleSignInClient;
+    private GoogleSignInAccount account;
 
     //func
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         viewModel = ViewModelProviders.of(requireActivity()).get(Test_ViewModel.class);
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestProfile()
+                .requestIdToken(Conf_Info.OAuth_CLIENT_TEST4)
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(getContext(),googleSignInOptions);
     }
 
     @Nullable
@@ -42,5 +59,60 @@ public class Test4_Firestore extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.test4_button1).setOnClickListener(this::test4_button1_onclick);
+        account = GoogleSignIn.getLastSignedInAccount(getContext());
+        ((TextView)view.findViewById(R.id.test4_textView1)).setText(account==null?"Null":account.getDisplayName());
+        ((SignInButton)view.findViewById(R.id.test4_google_signin)).setSize(SignInButton.SIZE_STANDARD);
+        view.findViewById(R.id.test4_google_signin).setOnClickListener(this::test4_googlelogin_onclick);
+        view.findViewById(R.id.test4_button2).setOnClickListener(this::test4_button2_onclick);
+    }
+
+    private void test4_button1_onclick(View view){
+//        util.quickLog(getActivity().toString());
+//        util.quickLog(getContext().toString());
+        account = GoogleSignIn.getLastSignedInAccount(getContext());
+        util.quickLog(account==null);
+    }
+
+    private void test4_googlelogin_onclick(View view){
+        Intent signInIntent = googleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent,6128);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 6128){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                account = task.getResult(ApiException.class);
+            } catch (ApiException e){
+                util.quickLog(e.getMessage());
+            }
+            ((TextView)getActivity().findViewById(R.id.test4_textView1)).setText(account==null?"Null":account.getDisplayName());
+        }
+    }
+
+    private void test4_button2_onclick(View view){
+        if (account==null){
+            util.quickLog("no google ac logined");
+        } else {
+            util.quickLog(account.getDisplayName());
+//            util.quickLog(account.getFamilyName());
+//            util.quickLog(account.getGivenName());
+            util.quickLog(account.getEmail());
+            util.quickLog(account.getId());
+            util.quickLog(account.getIdToken()==null?"Null":account.getIdToken());
+//            util.quickLog(account.getServerAuthCode());
+//            util.quickLog(account.getAccount()==null?"Null":"XNull");
+//            util.quickLog(account.getAccount().name);
+//            util.quickLog(account.getAccount().type);
+//            util.quickLog(account.getPhotoUrl().toString());
+            util.quickLog(account.getRequestedScopes().size());
+//            account.getRequestedScopes().forEach(scope -> util.quickLog(scope.toString()));
+            util.quickLog(account.getGrantedScopes().size());
+//            account.getGrantedScopes().forEach(scope -> util.quickLog(scope.toString()));
+        }
     }
 }
